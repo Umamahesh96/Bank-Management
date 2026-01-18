1e2d6cc715ee82ae796dd99dbcf8109f7962a536
mahesh <umamahesh.ch1995@gmail.com>
2026-01-17 23:11:19 -0500

    deposit and withdraw class using inheritance

---
+++ Main.java
@@ -0,0 +1,36 @@
+class Transaction{
+    private int customerId;
+    private int tellerId;
+    public Transaction(int customerId, int tellerId){
+        this.customerId = customerId;
+        this.tellerId = tellerId;
+    }
+    public int getCustomerId(){
+        return customerId;
+    }
+    public int getTellerId(){
+        return tellerId;
+    }
+    public String getTransactionDescription(){
+        return "";
+    }
+}
+class Deposit extends Transaction{
+    private int amount;
+    public Deposit(int customerId, int tellerId, int amount){
+        super(customerId, tellerId);
+        this.amount = amount;
+    }
+    @Override
+    public String getTransactionDescription(){
+        return "Teller "+getTellerId()+" deposited "+amount+" to account "+getCustomerId();
+    }
+}
+class Withdraw extends Transaction{
+    private int amount;
+    public Withdraw(int customerId, int tellerId, int amount){
+        super(customerId, tellerId);
+        this.amount = amount;
+    }
+    
+}
